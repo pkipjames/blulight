81748bdf9d01d9917ad7fa0645d9890835014b29
James M <45425618+pkipjames@users.noreply.github.com>
2019-01-14 07:35:16 -0500

    Update and rename pkipscriptbeta.js to scripts1.js

---
+++ scripts1.js
@@ -0,0 +1,11 @@
+$( "#dialog" ).dialog({
+  dialogClass: "no-close",
+  buttons: [
+    {
+      text: "OK",
+      click: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  ]
+});
